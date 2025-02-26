fade2171e0991a7d7ce9526e4999a593d07c4498
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-02-26 19:20:44 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,35 +1,35 @@
 provider "aws" {
-  region = "us-east-1" # Change to your preferred region
+  region = var.aws_region
 }
 
 # Create a VPC
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name   = "eks-vpc"
-  cidr   = "10.0.0.0/16"
+  cidr   = var.vpc_cidr
   
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
   enable_nat_gateway = true
 }
 
 # Create an EKS Cluster
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.27"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
   
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
   
   node_groups = {
     eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+      desired_capacity = var.desired_capacity
+      max_capacity     = var.max_capacity
+      min_capacity     = var.min_capacity
 
-      instance_types = ["t3.medium"]
+      instance_types = var.instance_types
     }
   }
 }
@@ -40,7 +40,7 @@ resource "null_resource" "deploy_python_app" {
 
   provisioner "local-exec" {
     command = <<EOT
-      aws eks --region us-east-1 update-kubeconfig --name my-eks-cluster
+      aws eks --region ${var.aws_region} update-kubeconfig --name ${var.cluster_name}
       kubectl apply -f https://github.com/younann/finalProject/blob/main/k8s/deployment.yaml
   }
 }
