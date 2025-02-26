00c4d97321dc6c87dd31b57f24b969ddc8568dff
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-02-26 19:21:36 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,24 @@
-# Define outputs here
-# Example:
-# output "instance_ip_addr" {
-#   value = aws_instance.example.public_ip
-# } 
+output "eks_cluster_id" {
+  description = "The ID of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_version" {
+  description = "Kubernetes version of the EKS cluster"
+  value       = module.eks.cluster_version
+}
+
+output "node_group_arns" {
+  description = "ARNs of the EKS worker nodes"
+  value       = module.eks.node_group_arns
+}
+
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
