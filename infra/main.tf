56173cd243d63e6bd1491c2c396d5f8ff58683ab
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-03 19:19:47 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,14 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
 
+resource "aws_iam_user" "dummy_user" {
+  name = "dummy-user-${uuid()}"  # Unique name for the user
+}
+
+output "dummy_user_name" {
+  value = aws_iam_user.dummy_user.name
+}
 
 /*
 provider "aws" {
