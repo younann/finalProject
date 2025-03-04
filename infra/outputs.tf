75810eed7320855d4f5a5c64eb7b9174ffbf869a
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 16:41:30 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,24 @@
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket for Terraform backend"
+  value       = aws_s3_bucket.terraform_state.bucket
+}
+
+output "dynamodb_table_name" {
+  description = "The name of the DynamoDB table for Terraform state locking"
+  value       = aws_dynamodb_table.terraform_lock.name
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
 output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
 }
 
 output "ecs_service_name" {
-  value = aws_ecs_service.app.name
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.app.name
 }
