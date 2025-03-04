79826113421579192e6fa3e67bf387fffd88de89
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 18:17:53 +0200

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,19 @@
-variable "aws_region" {
-  description = "AWS region for resources"
-  default     = "us-east-1"
-}
-
+# variables.tf
 variable "terraform_backend_bucket_name" {
   description = "S3 bucket name for Terraform state"
   type        = string
+  default     = "my-terraform-state-bucket" # Add a default value
 }
 
 variable "terraform_lock_table_name" {
   description = "DynamoDB table name for state locking"
   type        = string
+  default     = "my-terraform-lock-table" # Add a default value
 }
 
 variable "docker_image" {
   description = "Docker image URL for ECS container"
   type        = string
+  default     = "my-docker-repo/my-python-app:latest" # Add a default value
 }
 
