330e5bc61126f4b539352d7e499be7534538b23d
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 19:05:41 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,24 +1,20 @@
+# outputs.tf
 output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
+  value = aws_vpc.main.id
 }
 
 output "subnet_id" {
-  description = "Public subnet ID"
-  value       = aws_subnet.public.id
+  value = aws_subnet.public.id
 }
 
 output "ecs_cluster_name" {
-  description = "ECS Cluster Name"
-  value       = aws_ecs_cluster.main.name
+  value = aws_ecs_cluster.main.name
 }
 
 output "ecs_service_name" {
-  description = "ECS Service Name"
-  value       = aws_ecs_service.app.name
+  value = aws_ecs_service.app.name
 }
 
 output "load_balancer_dns" {
-  description = "Load Balancer DNS"
-  value       = aws_lb.app.dns_name
+  value = aws_lb.app.dns_name
 }
