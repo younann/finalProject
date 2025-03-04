a8eb93ec52215debdd9d620049ca660e097f22b4
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 13:59:47 +0200

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     encrypt        = true
   }
 }
