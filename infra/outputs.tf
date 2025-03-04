2f3a30ed0140abdff124b7260b8499a6ab3d7ce0
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 20:59:13 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,12 +1,24 @@
 output "vpc_id" {
-  value = module.ecs.vpc_id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
 output "ecs_cluster_name" {
-  value = module.ecs.ecs_cluster_name
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
 }
 
 output "ecs_service_name" {
-  value = module.ecs.ecs_service_name
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.app.name
 }
 
+output "ecs_task_definition_arn" {
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.app.arn
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.example.id
+}
