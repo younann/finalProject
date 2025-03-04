7b69c660ccb0d326a0cf718ef27c04c4cc2f4fb2
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-04 13:15:23 +0200

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,78 +1,23 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "private_key_path" {
-  default = "private_key.pem"
-}
-
-
 variable "aws_region" {
-  description = "The AWS region to deploy resources"
+  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
+variable "aws_access_key" {
+  description = "AWS Access Key"
   type        = string
-  default     = "my-eks-cluster"
+  sensitive   = true
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version for the EKS cluster"
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
   type        = string
-  default     = "1.27"
+  sensitive   = true
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "docker_image" {
+  description = "Docker image for ECS"
   type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "azs" {
-  description = "Availability Zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
-}
-
-variable "public_subnets" {
-  description = "Public subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnets" {
-  description = "Private subnet CIDR blocks"
-  type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
-}
-
-variable "instance_types" {
-  description = "EC2 instance types for EKS nodes"
-  type        = list(string)
-  default     = ["t3.medium"]
-}
-
-variable "desired_capacity" {
-  description = "Desired number of worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "min_capacity" {
-  description = "Minimum number of worker nodes"
-  type        = number
-  default     = 1
-}
-
-variable "max_capacity" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 3
-}
-
-variable "github_repo" {
-  description = "GitHub repository URL for Kubernetes deployment"
-  type        = string
-  default     = "https://github.com/younann/finalProject/blob/main/k8s/deployment.yaml"
+  default     = "my-python-app:latest"
 }
