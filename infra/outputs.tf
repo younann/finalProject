7b4c4708a1ad9c81ded1ec42e4a68329418d66a6
abedhaj25 <105980876+abedhaj25@users.noreply.github.com>
2025-03-07 14:34:28 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,4 +2,14 @@ output "load_balancer_dns" {
   value = aws_lb.app_alb.dns_name
 }
 
+output "eks_cluster_name" {
+  value = aws_eks_cluster.eks_cluster.name
+}
 
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app_bucket.bucket
+}
